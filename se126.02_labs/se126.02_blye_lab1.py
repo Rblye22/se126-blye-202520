b3ef2d36584c08e9876a92fac5d71c22f0314668
Rblye22 <rpblye@email.neit.edu>
2025-01-07 10:59:17 -0500

    se126_blye_lab1
    lab1 started in class, finish at home

---
+++ se126.02_labs/se126.02_blye_lab1.py
@@ -0,0 +1,23 @@
+#Robert Blye
+#SE126.02
+#Lab1
+#1-7-2025 [W1D2]
+
+#PROGRAM PROMPT: 
+#it is a program that determines whether a meeting room is in violation of fire regulations regarding the maximum room capacity. The program will accept the maximum room capacity and the number of people attending the meeting. If the number of people is less than or equal to the maximum room capacity, the program announces that it is legal to hold the meeting and tells how many additional people may legally attend. If the number of people exceeds the maximum room capacity, the program announces that the meeting cannot be held as planned due to the fire regulation and tells how many people must be excluded in order to meet the fire regulations. The user should be allowed to enter and check as many rooms as they would like without exiting the program.
+
+
+#VARIABLE DICTIONARY:
+
+#--------IMPORTS----------------------------------------------
+
+#--------FUNCTIONS--------------------------------------------
+def difference(people, max_cap):
+
+
+
+def decision(response):
+      
+
+
+#--------MAIN EXECUTING CODE----------------------------------
