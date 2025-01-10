d38a08cbac8aaeac7512195fc5ed935bad7d5d20
Rblye22 <rpblye@email.neit.edu>
2025-01-10 12:55:40 -0500

    Update se126.02_blye_lab1.py
    still need to fix a few things,  keeps reading invalid entry and is not displaying number difference.

--- se126.02_labs/se126.02_blye_lab1.py
+++ se126.02_labs/se126.02_blye_lab1.py
@@ -12,6 +12,9 @@
 #--------IMPORTS----------------------------------------------
 from os import system, name
 #--------FUNCTIONS--------------------------------------------
+max_cap = 1
+people = 1
+response = 0
 
 def clear():
     if name == 'nt': 
@@ -19,36 +22,60 @@ def clear():
     else:
          _ = system('clear')
 
-people = int(input("Enter ammount of people attending the meeting."))
-max_cap = int(input("Enter room max capacity"))
 
 def difference(people, max_cap):
     '''This is the function that tells the difference of people in the room and the max capacity.'''
-    people == int(input("Enter the amount of people attending the meeting"))
-    max_cap == int(input("Enter the max capacity for the room"))
     while people <= max_cap:
-        max_cap - people = answer + 1
+        open_seats = max_cap - people 
+
+        print(f"{open_seats} people can be added to the meeting and still meet fire regulations ")
+
         if people >= max_cap:
-            people - max_cap = answer - 1
+
+            ppl_over = people - max_cap
+            ppl_over + 1
+            print(f"{ppl_over} people must be removed from the meeting to meet fire regulations.")
+
+            return answer
 
 def decision(response):
-    '''This is the response to the amount of peope who need to be removed or can be added to the meeting.'''
-    while answer!="y" or answer !="Y":
-        if people <= max_cap:
-            print (f"You can add up to {answer} people")
+    '''This is the function to ask the user if they would like to continue to check the program for another meeting attendance information."'''
+
+    answer = input("\t\tWould you like to check another meeting? [y/n]: ").lower()
+    
+    while answer =="y" or answer =="n":
+        if answer == "y":
+            answer == "y"
+
+        if answer == "n":
+         answer != "y"
+
+        elif answer != "y" or answer != "n":
+            print("***INVALID ENTRY!***")
+            answer = input("\t\tWould you like to check anothe meeting? [y/n]: ").lower()
+
+            return answer
             
 
 
-
 #--------MAIN EXECUTING CODE----------------------------------
-#initializing needed variables
+
 clear()
 answer = "y"
 
-
 #start of loop - will be based on answer, and user can change value at end of loop
-while answer == "y" or answer=="Y":
+while answer == "y":
     print("\tWelcome to the Fire Department fire saftey program!")
+
     print(input("\tPlease enter your meeting name:"))
-    print(input("\tHow many people are attending this meeting?"))
-    print(input("\tWhat is the maximun capacity for that room?"))
+
+    print(int(input("\tHow many people are attending this meeting?")))
+
+    print(int(input("\tWhat is the maximun capacity for that room?")))
+
+    difference(people,max_cap)
+
+    decision(response)
+
+else:
+    print("Thank you for your complaince with our fire code")
