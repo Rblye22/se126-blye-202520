fb3e7bb15948b8b6ce635aab76222c61f0d5335f
Rblye22 <rpblye@email.neit.edu>
2025-01-12 18:48:30 -0500

    Update se126.02_blye_lab1.py
    Done and commented

--- se126.02_labs/se126.02_blye_lab1.py
+++ se126.02_labs/se126.02_blye_lab1.py
@@ -8,6 +8,15 @@
 
 
 #VARIABLE DICTIONARY:
+# max_cap - the maximun amount of people allowed in the meeting
+# people - the amount of people attending the meeting
+# respone - the choice the user is making
+# open_seats - seats available in the meeting
+# ppl_over - the amount of people over the limit of people allowed in the room
+# difference - the function to figure out the difference of people and the room capacity
+# decision - the function that asks the user if they want to check another room
+# clear - the function that clears the terminal 
+
 
 #--------IMPORTS----------------------------------------------
 from os import system, name
@@ -27,21 +36,21 @@ def clear():
 def difference(people, max_cap):
     '''This is the function that tells the difference of people in the room and the max capacity.'''
 
-    max_cap = (int(input("\n\tWhat is the maximun capacity for that room?")))
-    people = (int(input("\n\tHow many people are attending this meeting?")))
+    max_cap = (int(input("\n\tWhat is the maximun capacity for that room?"))) # users input for max capacity
+    people = (int(input("\n\tHow many people are attending this meeting?"))) # users input for people attending
 
     while answer =="y":  
     
-        if people <= max_cap:
+        if people <= max_cap: # if people is less than max capacity
             open_seats = max_cap - people 
 
-            print(f"\n\t{open_seats} people can be added to the meeting and still meet fire regulations.")
+            print(f"\n\t{open_seats} people can be added to the meeting and still meet fire regulations.") # people can be added
 
-        if people >= max_cap:
+        if people >= max_cap: #if people is greater than max capacity
             ppl_over = people - max_cap
             ppl_over + 1
 
-            print(f"\n\t{ppl_over} people must be removed from the meeting to meet fire regulations.")
+            print(f"\n\t{ppl_over} people must be removed from the meeting to meet fire regulations.") # remove people
 
         return answer
 
@@ -54,7 +63,7 @@ def decision(resp):
 
     while resp != "y" and resp != "n":
         print("***INVALID ENTRY!***")
-        resp = input("\n\tWould you like to check another meeting? [y/n]: ").lower()
+        resp = input("\n\tWould you like to check another meeting? [y/n]: ").lower() # invalid entry not accepted
 
         #if response == "n":
             #response != "y"
@@ -72,15 +81,15 @@ answer = "y"
 
 #start of loop - will be based on answer, and user can change value at end of loop
 while answer == "y":
-    print("\n\tWelcome to the Fire Department fire saftey program!")
+    print("\n\tWelcome to the Fire Department fire saftey program!") # welcome message
 
-    print(input("\n\tPlease enter your meeting name:"))
+    print(input("\n\tPlease enter your meeting name:")) # asks user for meeting name
 
-    difference(people,max_cap)
+    difference(people,max_cap) # calls for the differnce function
 
-    response = input("\n\tDo you want to go again again [y/n]: ")
-
+    response = input("\n\tDo you want to go again again [y/n]: ") # asks user if they want to check another meeting
     
-    answer = decision(response)
-print("\n\tThank you for your compliance! Have a great day!")
+    answer = decision(response) # calls for the decision functions
+
+print("\n\tThank you for your compliance! Have a great day!") # Goodbye message
 
