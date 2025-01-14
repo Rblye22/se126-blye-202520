4db181ee37345ab4db786b9f0c00f68bcf5bc8fd
Rblye22 <rpblye@email.neit.edu>
2025-01-14 10:44:20 -0500

    Create se126.02_blye_lab2.py
    needs to be done, just set up basic layout

---
+++ se126.02_labs/se126.02_blye_lab2.py
@@ -0,0 +1,26 @@
+# Robert Blye
+# SE126.02
+# Lab 2
+# 1-14-2025 [W2D2]
+
+# PROGRAM PROMPT: 
+# the program is to display the amount of rooms that are over capacity and display the amount of people it is over by.
+
+
+# VARIABLE DICTIONARY:
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
+#--------------Imports------------------
+import csv
+
+
+#-------------Functions-------------------
+
