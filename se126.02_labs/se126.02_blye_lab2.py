953d017dc8362bbece4cd148cf7583bfb47bea84
Rblye22 <rpblye@email.neit.edu>
2025-01-18 10:08:56 -0500

    test upload
    test

--- se126.02_labs/se126.02_blye_lab2.py
+++ se126.02_labs/se126.02_blye_lab2.py
@@ -21,6 +21,36 @@
 #--------------Imports------------------
 import csv
 
+total_records = 0 #the total number of recors (rows) in the file
 
-#-------------Functions-------------------
+print(f"\n{'TYPE':10} \t {'BRAND':3} \t {'CPU'} \t {'RAM'} \t {'DISK'} \t {'No HDD'} \t {'2ND Disk'} \t {'OS'} \t {'YR'}")
+#HEADER PRINT
+print("-----------------------------------")
 
+with open("") as csvfile:
+    #indent 1 level! (new block)
+
+    #allow processor to read the file data
+    file = csv.reader(csvfile)
+
+    #loop through every record(row) in the file
+    for record in file:
+
+        #add +1 to total_records
+        total_records += 1
+
+        print(record) #this is the list view of each record (row)
+
+        type = record [0]
+        brand = record[1]
+        cpu = record [2]
+        ram =  record [3]
+        one_disk = record [4]
+        no_hdd = record [5]
+        sec_disk = record [6]
+        os = record [7]
+        year = record [8]
+
+        print(f"{type:10} \t {brand:3} \t {cpu:3} \t {ram:3} \t {one_disk:3} \t {no_hdd:3} \t {sec_disk:3} \t {os:3} \t {year:3}")
+#-----------disconnected from file-------------
+print(f"\n TOTAL RECORDS: {total_records}\n")
