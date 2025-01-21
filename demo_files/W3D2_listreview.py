4acf0c1aedffbdb6a9de9f63a729080976c45a60
Rblye22 <rpblye@email.neit.edu>
2025-01-21 09:51:42 -0500

    W3D2 Demo
    In-class demo

---
+++ demo_files/W3D2_listreview.py
@@ -0,0 +1,71 @@
+#W3D2 - List Review - 1Dimensional List & Parallel List
+
+#this filr uses: class_grades.csv
+
+#---------Imports------------------------
+import csv
+
+#---------Functions----------------------
+
+#---------Main Executing Code------------
+
+total_records = 0\
+
+#create a list for every potenial field
+
+firstname = []
+lastname = []
+test1 = []
+test2 = []
+test3 = []
+
+with open ("text_files/class_grades.csv") as csvfile:
+
+    file = csv.reader (csvfile)
+
+    for rec in file:
+        #for every record in the file do the following
+        #print(f"{rec[0]:10}\t{rec[1]:10}")
+
+        #fname = rec[0]
+        #lname = rec [1]
+
+        #add the record data to its corresponding list (1 list per field)
+        #append--> to add to the end
+        firstname.append(rec [0])
+        lastname.append(rec[1])
+
+        test1.append(int(rec[2])) #cast as integer for easier math later
+        test2.append(int(rec[3]))
+        test3.append(int(rec[4]))
+
+# disconnect from the the file -- all file data is retained bc we are using lists
+
+#create a new list to hold each students test scores average
+avg = []
+
+#process the current students data to find and store each student's test score avg to the avg list
+for i in range(0, len(test1)):
+    a = (test1 + test2 +test3) / 3
+    avg.append(a)
+    #could also avg.append((test1[i] + test2[i] + test3[i]) /3)
+
+#basic procesing - use the 1D parallel lists to print all data to the console
+print(f"INDEX {'#':3} : {'FIRST':10} {'LAST':10}  {'T1':3}  {'T2':3}  {'T3':3} {'AVG':3}")
+print("-----------------------------------------------------------------------")
+
+for index in range (0, len(firstname)): #len() --> length of collection, returns # of items
+    #index --> key of the list, allows acess to ONE specific value
+
+    print(f"INDEX {index:3} : {firstname[index]:10} {lastname[index]:10}  {test1[index]:3}  {test2[index]:3}  {test3[index]:3}  {avg[index]:3.2f}")
+print("--------------------------------------------------------------------\n")
+
+total_avg = 0 
+
+#find current avg of the class by processing the avg list data
+for i in range(0,len (avg)):
+    total_avg += avg[i] #addes avg value to the total avg var
+
+    #calculate the average
+    class_avg = total_avg / len(avg)
+    print(f"\n THE CLASS AVERAGE of these {len(avg)} students is : {class_avg:.2f}")
--- se126.02_labs/se126.02_blye_lab2.py
+++ se126.02_labs/se126.02_blye_lab2.py
@@ -1,26 +1,66 @@
 # Robert Blye
 # SE126.02
 # Lab 2
-# 1-14-2025 [W2D2]
+# 1-18-2025 [W2D2]
 
 # PROGRAM PROMPT: 
-# the program is to display the amount of rooms that are over capacity and display the amount of people it is over by.
+# the program is to display the information from the text file and organize the information about the computers in the list. If the computer has a second hard drive then display that following information as well.
 
 
 # VARIABLE DICTIONARY:
-#
-#
-#
-#
-#
-#
-#
-#
-#
+# type - type of computer
+# brand - manufacturer of the computer
+# cpu - type of processor
+# ram - ram in gb
+# disk - size of hard drive
+# no_hdd - number of hard drives
+# os - operating system
+# disk2 - size of second hard drive
+# year - the year the computer was purchased
+# total-records - is the total records overall in the list 
 
 #--------------Imports------------------
 import csv
 
+total_records = 0 #the total number of recors (rows) in the file
 
-#-------------Functions-------------------
+print(f"\n{'TYPE'}\t {'BRAND'}\t     {'CPU'}\t {'RAM'}\t   {'DISK'}    {'No HDD'}\t {'2ND Disk'}    {'OS'}\t\t{'YR'}")
+#HEADER PRINT
+print("-------------------------------------------------------------------------------------------------------")
 
+with open("text_files/filehandling.csv") as csvfile:
+    #indent 1 level! (new block)
+
+    #allow processor to read the file data
+    file = csv.reader(csvfile)
+
+    #loop through every record(row) in the file
+    for record in file:
+
+        #add +1 to total_records
+        total_records += 1
+
+        type = record[0]
+        brand = record[1]
+        cpu = record[2]
+        ram =  record[3]
+        one_disk = record[4]
+        no_hdd = record[5]
+
+        if no_hdd == "1": # if it doesnt have a second drive
+            disk2 = " "
+            os = record[6] 
+            year = record[7]
+
+            #print(f"{type:10} {brand:10} {cpu:10} {ram:10} {one_disk:10}\t {no_hdd:10}\t {os:10}\t {year:10}")
+            print(f"{type:10} {brand:10} {cpu:10} {ram:10} {one_disk:10} {no_hdd:10} {os:10} {year:10}")
+        if no_hdd == "2": # if it has a second drive
+            disk2 = record[6]
+            os = record[7]
+            year = record[8]
+
+            print(f"{type:10} {brand:10} {cpu:10} {ram:10} {one_disk:10} {no_hdd:10} {disk2:10} {os:10} {year:10}")
+ # format wont line up how i want it to. i tried \t and tried tweeking the numbers and cant seem to figure it out. the short line still stays shorter than the longer line
+
+#-----------disconnected from file-------------
+print(f"\n TOTAL RECORDS: {total_records}\n")
