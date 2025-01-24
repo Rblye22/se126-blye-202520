2d4e2056781a52c6badc93e6a41a8dac9062692e
Rblye22 <rpblye@email.neit.edu>
2025-01-24 18:49:57 -0500

    updated inclass lab
    issue with year[index]

--- se126.02_labs/se126_blye_W3D2InclassLAb.py
+++ se126.02_labs/se126_blye_W3D2InclassLAb.py
@@ -1,7 +1,7 @@
 # Robert Blye
 # SE126.02
-# Lab 2
-# 1-18-2025 [W2D2]
+# W3D2 Inclass-lab
+# 1-24-25
 
 # PROGRAM PROMPT: 
 # the program is to display the information from the text file and organize the information about the computers in the list. If the computer has a second hard drive then display that following information as well.
@@ -17,17 +17,21 @@
 # os - operating system
 # disk2 - size of second hard drive
 # year - the year the computer was purchased
-# total-records - is the total records overall in the list 
-
+# total_records - is the total records overall in the list 
+# desk -  number of desktops
+# lap - number of laptops
+# com_total - cost to replace all computers
+# d_total - cost to replace all desktops
+# l_total - cost to replace all laptops
 #--------------Imports------------------
 import csv
 
 total_records = 0 #the total number of recors (rows) in the file
-
 print(f"\n{'TYPE'}\t {'BRAND'}\t     {'CPU'}\t {'RAM'}\t   {'DISK'}    {'No HDD'}\t {'2ND Disk'}    {'OS'}\t\t{'YR'}")
+#HEADER PRINT
 print("-------------------------------------------------------------------------------------------------------")
-#header print
 
+#-----initilizing------
 type = []
 brand = []
 cpu = []
@@ -42,15 +46,12 @@ lap = 0
 com_total = 0
 
 with open("text_files/filehandling.csv") as csvfile:
-  
+
     #allow processor to read the file data
     file = csv.reader(csvfile)
 
-    #loop through every record(row) in the file
+    #loop through every record in the file
     for rec in file:
-
-        #add +1 to total_records
-        total_records += 1
 
         type.append(rec[0])
         brand.append(rec[1])
@@ -59,40 +60,47 @@ with open("text_files/filehandling.csv") as csvfile:
         one_disk.append(rec[4])
         no_hdd.append(int(rec[5]))
 
+        desk = 0 # desktops
+        lap = 0 # laptops
+        com_total = 0 # overall cost
+        d_total = desk * 2000 # mult to get total cost for desktops
+        l_total = lap * 1500 # mult to get total cost for laptops
+
         if no_hdd == 1: # if it doesnt have a second drive
-            disk2.append (" ")
+            disk2.append(" ")
             os.append(rec[6])
             year.append(rec[7])
 
+           
         elif no_hdd == 2: # if it has a second drive
             disk2.append(rec[6])
             os.append(rec[7])
             year.append(rec[8])
 
-
-
     for index in range(0, len(type)):
         #index for computer types
     
-        if year[index] <= "16": 
+        if year[index] <= 16:
+            com_total = d_total + l_total # total cost to replace all computers if they choose to replace both desktops and laptops
 
-            if type[index] == "D": 
+            if type[index] == "D":  # number of desks tops that need to be replaced
                 desk += 1
                 
-            elif type[index] == "L": 
+            elif type[index] == "L":  # number of laptops that need to be replaced
                 lap += 1
 
-            print(f"{type[index]} {brand[index]} {cpu[index]} {ram[index]} {one_disk[index]} {no_hdd[index]} {disk2[index]} {os[index]} {year[index]}")
+        print(f"{type[index]:10} {brand[index]:10} {cpu[index]:10} {ram[index]:10} {one_disk[index]:10} {no_hdd[index]:10} {disk2[index]:10} {os[index]:10} {year[index]:10}") # list of computers
         
-d_total = desk * 2000
-l_total = lap * 1500
-com_total = d_total + l_total
-#-----------disconnected from file-------------
-print("\nBelow is the price it will cost to replace all systems that are made before 2016:\n")
-print(f"To replace {desk} desktops it will cost ${d_total}")
-print(f"To replace {lap} laptops it will cost ${l_total}")
-print(f"It will cost of total of ${com_total} to replace all systems.")
+d_total = desk * 2000 # cost to replace desktops made before 2016, num of desktops mult. by 2000
+l_total = lap * 1500  # cost to replace laptops made before 2016, num of laptops mult by 1500
 
-print("\n\tPress Enter To Continue...\n")
+#-----------disconnected from file-------------
+
+print("\nBelow is the price it will cost to replace all systems that are made before 2016 by type:\n") 
+print(f"To replace {desk} desktops it will cost ${d_total}") # showing price to replace desktops
+print(f"To replace {lap} laptops it will cost ${l_total}") # showing price to replace laptops
+
+input("\n\tPress Enter To Continue...\n")
+print(f"It will cost of total of ${com_total} to replace all systems.")
 
 
