c9c9cc4f3a1a2244a5ebecff6f27b7399f3d6eae
Rblye22 <rpblye@email.neit.edu>
2025-02-22 09:45:07 -0500

    update
    updating to run errands

--- se126_02_labs/se126_02_blye_lab6.py
+++ se126_02_labs/se126_02_blye_lab6.py
@@ -13,59 +13,33 @@
 
 import time
 import csv
-import os
-
 #----functions-------------------------------
 
-def display(x, foundList, records):
-    '''
-        PARAMETERS:
-            x   signifier for if we are printing a single record or multiple
-                when x != "x" it is an integere index and we have one value, otherwise we have multiple
-
-            records   the length of a list we are going to process through (# of loops/prints)
-    '''
-    print(f"{'Libary #':8}    {'TITLE':40}  {'Author':20}\t{'Grenre':15} {'Pages':13}\t{'STATUS':4}")
-    print("-------------------------------------------------------------------------------------------------------------------------------")
-    if x != "x":
-        #printing one record
-        print(f"{libNum[x]:8}  {title[x]:40}  {author[x]:20}  {genre[x]:20} {pages[x]:13} {status[x]:10}") 
-
-    elif foundList:
-        #printing multiples, based on length stored in 'foundList'
-        for i in range(0, records):
-            print(f"{libNum[foundList[i]]:8}  {title[foundList[i]]:40}  {author[foundList[i]]:20} {genre[foundList[i]]:20} {pages[foundList[i]]:13} {status[foundList[i]]:10}") 
-    
-    else:
-        #printing full data, based on length stored in 'records'
-        for i in range(0, records):
-            print(f"{libNum[i]:8}  {title[i]:40}  {author[i]:20}  {genre[i]:20} {pages[i]:13} {status[i]:10}")
+def display_seating_chart():
+    print("Airplane Seating Chart:")
+    for i in range(len(rowNum)):
+        print(f"{rowNum[i]} | {seatA[i]} {seatB[i]} {seatC[i]} {seatD[i]}")
 
 
-#--initalize---------------------------------------
+#--create lists------------------------------
 
-#--create empty lists------------------------------
+# Define the lists
+rowNum = ["1", "2", "3", "4", "5", "6", "7"]
+seatA = ["A", "A", "A", "A", "A", "A", "A"]
+seatB = ["B", "B", "B", "B", "B", "B", "B"]
+seatC = ["C", "C", "C", "C", "C", "C", "C"]
+seatD = ["D", "D", "D", "D", "D", "D", "D"]
 
-rowNum =[]
-seatA = []
-seatB = []
-seatC = []
-seatD = []
+# Create or open a text file in write mode ('w')
+with open('airplane_seating.txt', 'w') as file:
+    # Write a header line to the file (optional)
+    file = open('text_files/airplane_seating.csv' , 'w') # create text file 
 
-found = []
+for i in range(0, len(rowNum)):
+    file.write(f"{rowNum[i]},{seatA[i]},{seatB[i]},{seatC[i]},{seatD[i]}\n") # data for text file \n will add them to a new line at the end
+file.close() # close the file
 
-# opens the text file
-with open ("text_files/book_list.csv", encoding="utf-8") as csvfile:
-    file = csv.reader(csvfile)
 
-    for rec in file:
-        #append the file data into appropriate lists
-        # total record counter
-        libNum.append(rec[0]) # libary number append
-        title.append(rec[1]) # first name append
-        author.append(rec[2]) # last name append
-        genre.append(rec[3]) # age append
-        pages.append(rec[4]) # screen name append
-        status.append(rec[5])
-        
-    #--disconected from file-----------------------------
+display_seating_chart()
+seat_input = input("Enter the seat to reserve: ")
+
